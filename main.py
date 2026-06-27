88984950c10b626017363f4c7987a84919dd10d3
Keirin Tarou <seoo_japan_keirinoh@yahoo.co.jp>
2026-06-28 07:15:44 +0900

    refactor: main.py/不要なimport文を削除
    - 変更したファイル
        - modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import sys
-from pathlib import Path
 
 from PyQt6.QtWidgets import QApplication
 from PyQt6.QtGui import QIcon
 
 from app.ui.main_window import MainWindow
 from app.core.paths import get_base_dir
-from app.infrastructure.excel_runner import ExcelRunner
-from app.infrastructure.json_loader import JSONLoader
 
 TEST_QUERY = """
 SELECT
